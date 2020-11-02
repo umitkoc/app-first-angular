e8c022fa975a63c818783becc0bd670990d2d399
Ümit KOÇ <49067234+umitkoc@users.noreply.github.com>
2020-11-03 01:47:32 +0300

    lesson-2 model binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-desktop',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-first-angular';
+  user = 'Umit';
+  Merhaba="Hello ";
+  items=[
+    {Id:1,Desc:"Kahvaltı",Action:"No"},
+    {Id:3,Desc:"Sinema",Action:"Yes"},
+    {Id:6,Desc:"Spor",Action:"No"},
+    {Id:2,Desc:"Kitap",Action:"Yes"},
+    {Id:5,Desc:"Kahvaltı",Action:"No"}
+  ];
 }
